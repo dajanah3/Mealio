6b137e1bc8d7d082865988cb00e37cb96774620e
Charles Phan <charlesdphan@gmail.com>
2026-05-03 23:54:10 -0400

    AdView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.android.gms:play-services-ads:25.1.0")
 }
