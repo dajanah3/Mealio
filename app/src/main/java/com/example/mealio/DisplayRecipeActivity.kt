495b33bc1ffb718523e7ae1e077682465d66d284
dajanah3 <dajanahlu@gmail.com>
2026-05-06 21:26:04 -0400

    display recipe done

--- app/src/main/java/com/example/mealio/DisplayRecipeActivity.kt
+++ app/src/main/java/com/example/mealio/DisplayRecipeActivity.kt
@@ -1,14 +1,17 @@
 package com.example.mealio
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DisplayRecipeActivity : AppCompatActivity() {
     private lateinit var recipe_name : TextView
     private lateinit var ingredients : TextView
+    private lateinit var description : TextView
     private lateinit var steps : TextView
     private lateinit var share : Button
     private lateinit var back : ImageButton
@@ -19,6 +22,7 @@ class DisplayRecipeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display)
         recipe_name = findViewById<TextView>(R.id.recipe_name)
         ingredients = findViewById<TextView>(R.id.ingredients)
+        description = findViewById<TextView>(R.id.description)
         steps = findViewById<TextView>(R.id.steps)
 
         share = findViewById<Button>(R.id.share)
@@ -27,5 +31,20 @@ class DisplayRecipeActivity : AppCompatActivity() {
 
         back.setOnClickListener { finish() }
         favorite.setOnClickListener { MainActivity.mealio!!.favorite_recipe(recipe_name.text.toString()) }
+
+        val recipe_key = intent.getStringExtra("recipe")
+        val name = recipe_key!!.substringBefore(":")
+        recipe_name.text = name
+
+        val db : FirebaseFirestore = MainActivity.mealio!!.get_db()
+
+        db.collection("recipes").document(recipe_key).get()
+            .addOnSuccessListener { doc ->
+                if (doc.exists()) {
+                    description.text = (doc.get("description") as CharSequence?)
+                    ingredients.text = (doc.get("ingredients") as? List<String>)?.joinToString("\n") ?: ""
+                    steps.text = (doc.get("instructions") as? List<String>)?.joinToString("\n") ?: ""
+                }
+            }
     }
 }
--- app/src/main/java/com/example/mealio/Mealio.kt
+++ app/src/main/java/com/example/mealio/Mealio.kt
@@ -6,7 +6,9 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObjects
@@ -44,6 +46,10 @@ class Mealio (private val context : Context) {
                     }
                 }
         }
+    }
+
+    fun get_db(): FirebaseFirestore {
+        return db
     }
 
     fun save_recipe(name: String, recipe: HashMap<String, Any>, collection: String) {
